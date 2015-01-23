e51eda45ea2814395085b8b3dbf00771a1a69b8a
fblife-zhang <zhangshaonan@fblife.com>
2015-01-23 11:02:14 +0800

    11111
    111111

---
+++ Controls/MessageViewController/MessageViewController.m
@@ -0,0 +1,51 @@
+//
+//  MessageViewController.m
+//  CustomNewProject
+//
+//  Created by soulnear on 15-1-23.
+//  Copyright (c) 2015年 FBLIFE. All rights reserved.
+//
+
+#import "MessageViewController.h"
+
+@interface MessageViewController ()
+
+@end
+
+@implementation MessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.leftImageName = NAVIGATION_MENU_IMAGE_NAME2;
+    self.myTitle = @"我的消息";
+    self.isAddGestureRecognizer = YES;
+    
+    [self setMyViewControllerLeftButtonType:MyViewControllerLeftbuttonTypeOther WithRightButtonType:MyViewControllerRightbuttonTypeNull];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+}
+
+-(void)leftButtonTap:(UIButton *)sender
+{
+    [self.airViewController showAirViewFromViewController:self.navigationController complete:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
